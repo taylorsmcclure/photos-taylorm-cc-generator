e7580efbbf853fa55de32956d1f5fe4908a2d6e7
Taylor McClure <taylorsmcclure@gmail.com>
2017-12-08 12:24:17 -0800

    updated new param region for s3 url formation.

--- generator.py
+++ generator.py
@@ -1,50 +1,66 @@
 #!/usr/bin/env python3
 
 import argparse
-import botocore
 import boto3
 
 
 class TemplateGenerator:
 
-    def __init__(self, bucket, log_level):
+    def __init__(self, bucket, log_level, out_path, region):
         self.bucket = bucket
         self.log_level = log_level
+        self.out_path = out_path
+        self.region = region
         self.bucket_conn = self.s3_bucket()
-
+        self.obj_dict = {}
         self.template_gen()
 
     def s3_bucket(self):
         """
         establishes and connects to the bucket
-        :param bucket: s3 bucket to access
         :return: s3 bucket object
         """
         s3 = boto3.resource('s3')
         return s3.Bucket(self.bucket)
 
-    def iter_objects(self):
+    def parse_objects(self):
         """
         iterates through the objects in the given bucket
-        :return:
+        :return: none, but updates self.obj_dict
         """
         for obj in self.bucket_conn.objects.filter(Prefix="images"):
-            print(obj)
+            if obj.size != 0:
+                obj_full_path = obj.key
+                obj_file = obj.key.split("/")
+                obj_album = obj_file[1]
+                obj_key = obj_file[2]
+                try:
+                    self.obj_dict[obj_album]["keys"].append({obj_key: "url"})
+                except KeyError:
+                    self.obj_dict.update({obj_album: {"keys":[obj_key]}})
+            else:
+                pass
+
+        return self.obj_dict
 
     def template_gen(self):
-        self.iter_objects()
-        
+        print(self.parse_objects())
+
 
 def main():
-    TemplateGenerator(args.bucket, args.log_level)
+    TemplateGenerator(args.bucket, args.log_level, args.output_path, args.region)
 
 
 if __name__ == "__main__":
-    # Optional arguments for usability
+    # Arguments for usability
     parser = argparse.ArgumentParser(description="Generates templates for phugo from an s3 bucket")
     parser.add_argument("bucket", help="s3 bucket to read from e.g. img.taylorm.cc")
     parser.add_argument("--log-level", choices=["DEBUG", "INFO", "ERROR", "CRITICAL"],
                         default="ERROR", help="Chose the loglevel from the available choices; default is ERROR.")
+    parser.add_argument("--output-path", default="pwd",
+                        help="Choose the output directory, this would be the \"content\" directory.")
+    parser.add_argument("--region", default="us-west-1",
+                        help="Specify region so S3 full url is valid for object(s)")
 
     args = parser.parse_args()
 
